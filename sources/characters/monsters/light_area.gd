7860bcd2c0aa3659387f5ff9e151dd9ebf6e2829
Laurent Tourte <laurentourte@gmail.com>
2021-11-22 11:03:30 +0100

    Monsters freeze longer when exiteing the light

--- sources/characters/monsters/light_area.gd
+++ sources/characters/monsters/light_area.gd
@@ -3,10 +3,17 @@ class_name LightArea
 
 signal light(value)
 
+onready var timer: = $Timer
+
 
 func light_on() -> void:
 	emit_signal("light", true)
+	timer.stop()
 
 
 func light_off() -> void:
+	timer.start()
+
+
+func _on_Timer_timeout() -> void:
 	emit_signal("light", false)
