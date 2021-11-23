dfa52b0f95a7cecbead44cc02c893e96c58fc890
Laurent Tourte <laurentourte@gmail.com>
2021-11-23 10:58:11 +0100

    Added all physics layer to singleton

--- sources/utils/singletons/physics_layers.gd
+++ sources/utils/singletons/physics_layers.gd
@@ -8,12 +8,34 @@ class PhysicsLayer:
 		bit = n
 		number = 1 << n - 1
 
-# Other utils areas
+# Bodies
+var world: = PhysicsLayer.new(1)
+var characters: = PhysicsLayer.new(2)
+var rope_collider: = PhysicsLayer.new(3)
+
+# Areas
+var player: = PhysicsLayer.new(9)
+var monsters: = PhysicsLayer.new(10)
+var interactive_objects: = PhysicsLayer.new(11)
+
 var rope_length_limit: = PhysicsLayer.new(16)
+
+# Other utils areas
+var camera_focus: = PhysicsLayer.new(17)
 
 
 func _ready() -> void:
+	assert(ProjectSettings.get_setting("layer_names/2d_physics/layer_" + str(world.bit)) == "World")
+	assert(ProjectSettings.get_setting("layer_names/2d_physics/layer_" + str(characters.bit)) == "Characters")
+	assert(ProjectSettings.get_setting("layer_names/2d_physics/layer_" + str(rope_collider.bit)) == "RopeCollider")
+	
+	assert(ProjectSettings.get_setting("layer_names/2d_physics/layer_" + str(player.bit)) == "Player")
+	assert(ProjectSettings.get_setting("layer_names/2d_physics/layer_" + str(monsters.bit)) == "Monsters")
+	assert(ProjectSettings.get_setting("layer_names/2d_physics/layer_" + str(interactive_objects.bit)) == "InteractiveObjects")
+	
 	assert(ProjectSettings.get_setting("layer_names/2d_physics/layer_" + str(rope_length_limit.bit)) == "RopeLengthLimit")
+	
+	assert(ProjectSettings.get_setting("layer_names/2d_physics/layer_" + str(camera_focus.bit)) == "CameraFocus")
 
 
 func add_physics_layer(layers: int, other_layer: PhysicsLayer) -> int:
