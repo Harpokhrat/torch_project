18d95def68e71aa57b874cf3e168ea279865a0c4
Laurent Tourte <laurentourte@gmail.com>
2021-11-25 15:40:28 +0100

    Added visibility enablers

--- sources/objects/thunder_window_pool.gd
+++ sources/objects/thunder_window_pool.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 onready var timer: Timer = $Timer
-onready var thunder: = $Thunder2
+onready var thunder: = $Thunder
 
 var audio_resource_array: Array = [
 	"res://resources/sfx/Thunder/Thunder1.ogg","res://resources/sfx/Thunder/Thunder2.ogg",
@@ -9,7 +9,6 @@ var audio_resource_array: Array = [
 	"res://resources/sfx/Thunder/Thunder5.ogg","res://resources/sfx/Thunder/Thunder6.ogg"
 ]
 var window_list: Array = []
-var thunder_done = 0
 
 
 
@@ -17,20 +16,11 @@ func _ready() -> void:
 	for child in get_children():
 		if child is Window:
 			window_list.append(child)
-			
-			var _a = child.connect("thunder_done", self, "thunder_is_done")
 	
-	_randomize_thunder()
-
-
-func _process(_delta: float) -> void:
-	if thunder_done >= len(window_list):
-		_randomize_thunder()
+	timer.start(rand_range(7, 21))
 
 
 func _randomize_thunder() -> void:
-	thunder_done = 0
-	
 	var pitch_scale = rand_range(0.9, 1.5)
 	var volume = rand_range(-6, 0)
 	
@@ -41,15 +31,12 @@ func _randomize_thunder() -> void:
 	thunder.volume_db = volume
 	thunder.stream = audio_stream
 	
-#	timer.start(1)
-	timer.start(rand_range(3, 8))
-
-
-func _on_Timer_timeout() -> void:
 	for window in window_list:
 		window.play_thunder()
 		thunder.play()
+	
+	timer.start(rand_range(7, 21))
 
 
-func thunder_is_done() -> void:
-	thunder_done += 1
+func _on_Timer_timeout() -> void:
+	_randomize_thunder()
