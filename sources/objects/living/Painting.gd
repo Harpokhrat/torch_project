118d6563e18bba35a5604e3162e2d0d72c87df91
BimDav <67792882+BimDav@users.noreply.github.com>
2021-11-29 16:31:22 +0100

    mini fix to warning

--- sources/objects/living/Painting.gd
+++ sources/objects/living/Painting.gd
@@ -4,7 +4,7 @@ onready var hitbox_collision: = $HitBox/CollisionShape2D
 
 
 func _on_LightArea_light(value) -> void:
-	hitbox_collision.disabled = value
+	hitbox_collision.set_deferred("disabled", value)
 	if not _globals.first_painting_lit_up:
 		_globals.first_painting_lit_up = true
 		_globals.emit_signal("dialog", "Better light up these weird paintings before passing in front of them!", {"center": ""})
