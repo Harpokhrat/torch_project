4f2b2dfdad32b489536a68cd3583ab3e121c62c9
Raphael Lanthiez <raphael.lanthiez@gmail.com>
2021-11-29 18:24:54 +0100

    [Sound]
    Musiques Intro Final et Menu

--- sources/scenes/link_button.gd
+++ sources/scenes/link_button.gd
@@ -1,5 +1,8 @@
 extends LinkButton
 
+onready var sound_click: = $ClickMenu
 
 func _on_DiscordLink_pressed() -> void:
+	sound_click.play()
 	var _a: = OS.shell_open("https://discord.gg/PUdRGyvPAb")
+	
