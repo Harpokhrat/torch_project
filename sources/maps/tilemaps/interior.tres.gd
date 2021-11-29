1e1755793d5413e2b8e7724e533764914094cc9f
Laurent Tourte <laurentourte@gmail.com>
2021-11-29 16:08:03 +0100

    Added library to the tileset

--- sources/maps/tilemaps/interior.tres.gd
+++ sources/maps/tilemaps/interior.tres.gd
@@ -1,8 +1,10 @@
 tool
 extends TileSet
 
+var neighbor_tilesets = [0, 1, 2]
+
 
 func _is_tile_bound(drawn_id: int, neighbor_id: int) -> bool:
-	if drawn_id in [0, 1] and neighbor_id in [0, 1]:
+	if drawn_id in neighbor_tilesets and neighbor_id in neighbor_tilesets:
 		return true
 	return false
