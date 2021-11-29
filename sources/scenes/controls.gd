f56a08c3de78baf65599a2bda5f71b56a6cdcc28
Laurent Tourte <laurentourte@gmail.com>
2021-11-29 18:47:36 +0100

    Added control screen

---
+++ sources/scenes/controls.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("ui_cancel"):
+		load_game()
+
+
+func load_game() -> void:
+	get_tree().change_scene("res://sources/scenes/school.tscn")
+
+
+func _on_Timer_timeout() -> void:
+	load_game()
