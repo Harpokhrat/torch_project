9eeac3f6b3c9c4d3fd0f27ae78494b0eb6e979b6
BimDav <67792882+BimDav@users.noreply.github.com>
2021-11-29 16:58:06 +0100

    new paintings

--- sources/objects/living/Painting.gd
+++ sources/objects/living/Painting.gd
@@ -1,10 +1,20 @@
 extends Node2D
 
 onready var hitbox_collision: = $HitBox/CollisionShape2D
+onready var animation_player: = $AnimationPlayer
 
 
 func _on_LightArea_light(value) -> void:
-	hitbox_collision.set_deferred("disabled", value)
+	if value:
+		hitbox_collision.set_deferred("disabled", value)
+		animation_player.play("lit")
+	else:
+		animation_player.play("unlit")
 	if not _globals.first_painting_lit_up:
 		_globals.first_painting_lit_up = true
 		_globals.emit_signal("dialog", "Better light up these weird paintings before passing in front of them!", {"center": ""})
+
+
+func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
+	if anim_name == "unlit":
+		hitbox_collision.disabled = false
